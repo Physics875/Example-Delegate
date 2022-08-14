b80e7385e1c5320736fe9cf9e288ab957111e4d5
Physics850 <108977048+Physics875@users.noreply.github.com>
2022-08-14 15:00:38 +0700

    Update Delegate_Example.cs

--- Delegate_Example.cs
+++ Delegate_Example.cs
@@ -1,4 +1,5 @@
 //Về Delegate: nó giống như việc bạn giao cho một tổ chức khác (hàm, biến,...) làm thay nhiệm vụ thuộc về trách nhiệm của mình.
+// hàm delegate cũng có kiểu trả về, việc gọi hàm cũng tương tự nhưng chỉ khác là nó thông qua một field. 
 //vd:
 class Overview
 {
