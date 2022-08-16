1a076a57eeaf54598d31525f5992ba5ca43acb6e
Physics850 <108977048+Physics875@users.noreply.github.com>
2022-08-16 11:41:57 +0700

    Update Delegate_Example.cs

--- Delegate_Example.cs
+++ Delegate_Example.cs
@@ -1,13 +1,14 @@
 //Về Delegate: nó giống như việc bạn giao cho một tổ chức khác (hàm, biến,...) làm thay nhiệm vụ thuộc về trách nhiệm của mình.
 // hàm delegate cũng có kiểu trả về, việc gọi hàm cũng tương tự nhưng chỉ khác là nó thông qua một field. 
 //vd:
+using UnityEngine;
 class Overview
 {
 	public delegate void GetDelegate();
 	
 	private GetDelegate delegatedFunction;
 	  
-    static void Main(string[] args)
+    	private void Start()
 	{
 		delegatedFunction = DelegateFunction;
 	    	delegateFunction += SecondDelegateFunction;
@@ -19,13 +20,13 @@ class Overview
 	  
 	private void DelegateFunction()
 	{  
-		Console.Write("Will DelegateFunction");
+		Debug.Log("Will DelegateFunction");
 	}
 	
 	
 	private void SecondDelegateFunction()
 	{  
-		Console.Write("Will SecondDelegateFunction");
+		Debug.Log("Will SecondDelegateFunction");
 	}
 	
 	/* Output: 	Will DelegateFunction
